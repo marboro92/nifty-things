9ff0137fcadd8bb08ffce00e994c6493d910edd4
Marianne Borowiak <marianne.borowiak@outcomes4me.com>
2022-01-31 10:48:05 -0600

    set up base theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,54 @@ module.exports = {
       sans: ['Source Sans Pro', 'sans-serif'],
       head: ['Source Sans Pro', 'sans-serif'],
     },
+    spacing: {
+      half: '5px',
+      1: '10px',
+      2: '20px',
+      3: '30px',
+      4: '40px',
+      5: '50px',
+      6: '60px',
+    },
+  },
+  daisyui: {
+    themes: [
+      {
+        light: {
+          /* your theme name */ primary: '#5D5FEF' /* Primary color */,
+          'primary-focus': '#2A2CBC' /* Primary color - focused */,
+          'primary-content':
+            '#ffffff' /* Foreground content color to use on primary color */,
+
+          secondary: '#51596D' /* Secondary color */,
+          'secondary-focus': '#1E263A' /* Secondary color - focused */,
+          'secondary-content':
+            '#ffffff' /* Foreground content color to use on secondary color */,
+
+          accent: '#EF625D' /* Accent color */,
+          'accent-focus': '#d1443f' /* Accent color - focused */,
+          'accent-content':
+            '#ffffff' /* Foreground content color to use on accent color */,
+
+          neutral: '#51596D' /* Neutral color */,
+          'neutral-focus': '#1E263A' /* Neutral color - focused */,
+          'neutral-content':
+            '#ffffff' /* Foreground content color to use on neutral color */,
+
+          'base-100':
+            '#ffffff' /* Base color of page, used for blank backgrounds */,
+          'base-200': '#f9fafb' /* Base color, a little darker */,
+          'base-300': '#d1d5db' /* Base color, even more darker */,
+          'base-content':
+            '#1C1C1C' /* Foreground content color to use on base color */,
+
+          info: '#2094f3' /* Info */,
+          success: '#60EF5D' /* Success */,
+          warning: '#ff9900' /* Warning */,
+          error: '#EF625D' /* Error */,
+        },
+      },
+    ],
   },
   plugins: [require('daisyui'), require('@tailwindcss/typography')],
 }
