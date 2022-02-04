15536353a477537a98ec233811b6cf06f14bc995
Marianne Borowiak <marboro92@gmail.com>
2022-02-04 16:18:47 -0600

    update layout scrolling

--- components/Layout.js
+++ components/Layout.js
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { MenuButton, PillButton } from './buttons'
 import Head from './Head'
 import Sidebar from './sidebar/Sidebar'
+import Player from './player/Player'
 
 const Layout = ({ children }) => {
   const [open, setOpen] = useState(false)
@@ -12,29 +13,32 @@ const Layout = ({ children }) => {
   return (
     <>
       <Head />
-      <div className="flex h-screen divide-x divide-base-300">
-        <Sidebar open={open} />
-        <main className="divide-y divide-base-300">
-          <div className="flex px-1 py-1 gap-1 flex-wrap">
-            <MenuButton
-              open={open}
-              onClick={onClickMenu}
-              className="md:hidden"
-            />
-            <div className="grow flex gap-1 items-center justify-end sm:grow-0 sm:order-last">
-              <Link href="/profile">
-                <PillButton as="a">Profile</PillButton>
-              </Link>
-              <PillButton secondary>Connect Wallet</PillButton>
+      <div className="h-screen flex flex-col">
+        <div className="flex h-[calc(100%-80px)] divide-x divide-base-300">
+          <Sidebar open={open} />
+          <main className="h-full w-full divide-y divide-base-300 overflow-auto">
+            <div className="flex px-1 py-1 gap-1 flex-wrap">
+              <MenuButton
+                open={open}
+                onClick={onClickMenu}
+                className="md:hidden"
+              />
+              <div className="grow flex gap-1 items-center justify-end sm:grow-0 sm:order-last">
+                <Link href="/profile">
+                  <PillButton as="a">Profile</PillButton>
+                </Link>
+                <PillButton secondary>Connect Wallet</PillButton>
+              </div>
+              <input
+                type="text"
+                placeholder="Search music, collections, accounts"
+                className="input input-bordered flex-1 min-w-[200px]"
+              />
             </div>
-            <input
-              type="text"
-              placeholder="Search music, collections, accounts"
-              className="input input-bordered flex-1 min-w-[200px]"
-            />
-          </div>
-          <div className="h-full">{children}</div>
-        </main>
+            <div className="w-full">{children}</div>
+          </main>
+        </div>
+        <Player />
       </div>
     </>
   )
--- pages/profile.js
+++ pages/profile.js
@@ -1,11 +1,13 @@
 import Layout from '../components/Layout'
+import { Body1 } from '../components/typography'
 import H1 from '../components/typography/H1'
 
 const ProfilePage = () => {
   return (
     <Layout>
-      <div className="flex h-full">
+      <div className="h-full mx-2 my-2 md:mx-5 md:my-3">
         <H1>Profile Settings.</H1>
+        <Body1>Update your profile here.</Body1>
       </div>
     </Layout>
   )
