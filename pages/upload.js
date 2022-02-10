88065d75fc8e6a1da19b4ca073ddc59341db9038
Marianne Borowiak <marboro92@gmail.com>
2022-02-10 09:41:33 -0600

    Add upload page

--- pages/upload.js
+++ pages/upload.js
@@ -4,18 +4,18 @@ import Layout from '../components/Layout'
 import { Body1 } from '../components/typography'
 import H1 from '../components/typography/H1'
 
-const ProfilePage = () => {
+const UploadPage = () => {
   return (
     <Layout>
       <div className="h-full mx-2 my-2 md:mx-5 md:my-3">
-        <H1>Profile Settings.</H1>
-        <Body1>Update your profile here.</Body1>
-        <Link href="/upload">
-          <PillButton secondary>Upload</PillButton>
-        </Link>
+        <H1>Upload Files.</H1>
+        <Body1>
+          Upload your music and artwork. Provide metadata of NFT items. Generate
+          Content ID.
+        </Body1>
       </div>
     </Layout>
   )
 }
 
-export default ProfilePage
+export default UploadPage
