162f86697c2394d4065b76dc2f6e1472c8395843
Marianne Borowiak <marboro92@gmail.com>
2022-03-09 09:31:38 -0600

    fix logo cursor

--- components/Nav.js
+++ components/Nav.js
@@ -6,8 +6,8 @@ const Nav = ({ children, homeHref, className = '' }) => (
       className={`flex z-nav h-[80px] items-center justify-between fixed bg-base-100 w-full ${className}`}
     >
       <nav className="md:flex w-full items-center justify-between mx-auto px-1 max-w-[1236px]">
-        <Link href={homeHref}>
-          <h1 className="font-bold text-[24px]">NiftyTunes</h1>
+        <Link href={homeHref} passHref>
+          <a className="font-bold text-[24px]">NiftyTunes</a>
         </Link>
         <div className="flex items-center justify-center space-x-3">
           {children}
