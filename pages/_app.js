f3c2e7abda0c1f5799d7e5e72236ad9cece6628b
Marianne Borowiak <marboro92@gmail.com>
2022-06-15 10:43:49 -0400

    rm logrocket

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,6 @@ import { configureAmplify } from '../utils/cognito'
 import '../styles/globals.css'
 import { ArtistUserProvider } from '../contexts/ArtistUserContext'
 import { ArtistCollectionsProvider } from '../contexts/ArtistCollectionsContext'
-import LogRocket from 'logrocket'
-
-if (process.env.NEXT_PUBLIC_ENV !== 'local') {
-  LogRocket.init('rnmr6s/nifty')
-}
 
 configureAmplify()
 
