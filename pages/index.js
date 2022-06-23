d7c55b02fdfb5c039cf6920ecb2c38800c5da1fd
Marianne Borowiak <marboro92@gmail.com>
2022-06-23 17:40:19 -0400

    clean up dead code

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import { useNFTCollection } from '@thirdweb-dev/react'
 import { useAddress } from '@thirdweb-dev/react'
-import { ThirdwebSDK } from '@thirdweb-dev/sdk'
 import Layout from '../components/Layout'
 import Release from '../components/Release'
 import content from '../content/marketplace/discover.json'
@@ -8,14 +6,12 @@ import { useEffect, useState } from 'react'
 import { claimNft, getAllAvailableNfts } from '../utils/contract'
 
 const HomePage = () => {
-  const contract = useNFTCollection(process.env.NEXT_PUBLIC_SMART_CONTRACT)
   const [collections, setCollections] = useState()
   const address = useAddress()
   const collectionOriginalOwner = process.env.NEXT_PUBLIC_DEFAULT_OWNER
 
   const loadNFTCollections = async () => {
     const nfts = await getAllAvailableNfts()
-    console.log(JSON.stringify(nfts))
     setCollections(nfts)
   }
 
@@ -34,9 +30,6 @@ const HomePage = () => {
 
   return (
     <Layout>
-      {/* <div className="mx-2 mt-3 md:mt-[80px]">
-        <H1 size="md">{content.title}</H1>
-      </div> */}
       <h3 className="font-bold text-[1.5rem] px-2 pt-2 md:pt-5 pb-2">
         {content.subtitle}
       </h3>
